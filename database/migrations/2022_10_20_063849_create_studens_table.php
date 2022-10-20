e4837ef72205842d2189e941a642e32b1e94bb0e
Megyeri Márk <megyko1234@gmail.com>
2022-10-20 08:46:42 +0200

    Kezdés

---
+++ database/migrations/2022_10_20_063849_create_studens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('studens', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("email");
+            $table->string("phone");
+            $table->foreignId("course_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('studens');
+    }
+};
