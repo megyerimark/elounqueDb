bcf99e29bfcafe08ddcb4d61d94a436a9180df30
Megyeri Márk <megyko1234@gmail.com>
2022-10-20 11:49:59 +0200

    Shools tabla megíirasa

--- database/migrations/2022_10_20_094610_create_schools_table.php
+++ database/migrations/2022_10_20_094610_create_schools_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('schools', function (Blueprint $table) {
             $table->id();
+            $table->string("school");
+            $table->foreigId("course_id");
             $table->timestamps();
         });
     }
