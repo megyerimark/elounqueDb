680703f5207f628f2815d36911f56b714cd9bcb0
Megyeri Márk <megyko1234@gmail.com>
2022-10-20 10:21:20 +0200

    Végre működik minden --version 0.0.5

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -4,29 +4,41 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Student;
+use App\Models\Course;
 
 class SchoolController extends Controller
 {
-    public function listStudent(){
+//     public function listStudent(){
 
-        $students = Student::with('course')->get();
+//         $students = Student::with('course')->get();
     
        
-        // foreach($students as $student){
+//         // foreach($students as $student){
 
         
-        // echo "<pre>";
+//         // echo "<pre>";
   
-        //  print_r($student->name. " ");
-        //  print_r($student->course->course);
-        // }
+//         //  print_r($student->name. " ");
+//         //  print_r($student->course->course);
+//         // }
 
+
+//         return view ("student",[
+//             "students" => $students
+//         ]);
+
+
+
+//     }
+
+   public function listStudent(){
+
+        $students = Course::find( 1 ) ->student;
 
         return view ("student",[
-            "students" => $students
-        ]);
-
-
+                        "students" => $students
+                     ]);
 
     }
+
 }
