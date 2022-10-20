ea480f0c4e6c2c274172f7d62ee86eb667329600
Megyeri Márk <megyko1234@gmail.com>
2022-10-20 09:15:19 +0200

    modellek javítása --version 0.0.2

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use\App\Models\StudentModel;
 
 class SchoolController extends Controller
 {
-    //
+    public function listStudent(){
+
+        $student = Student::with("course")->get();
+
+    }
 }
